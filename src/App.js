1ef57d08d09c862f505b21a3c5dba6d2c967306e
Jose Maria Zamora Perez <jzamora@biada.es>
2021-06-23 11:43:18 +0200

    examen funcional pero falta pulir muchas cosas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect } from "react";
+import { useState } from "react";
+import { Formulario } from "./componentes/Formulario";
+import { Tarjeta } from "./componentes/Tarjeta";
 
 function App() {
+  const urlApi = "http://localhost:3001/amigos/";
+  const [amigos, setAmigos] = useState([]);
+
+  const numeroAmigos = amigos.length;
+  const [editar, setEditar] = useState(false);
+
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [valoracion, setValoracion] = useState(0);
+  const [modificar, setModificar] = useState(false);
+  const [id, setId] = useState(0);
+  const llamadaAmigosApi = async (url) => {
+    const response = await fetch(url);
+    const amigosApi = await response.json();
+    setAmigos(amigosApi);
+  };
+  useEffect(() => llamadaAmigosApi(urlApi), []);
+  const toggleEditar = (e) => {
+    e.preventDefault();
+    setEditar(!editar);
+  };
+
+  const borrarAmigo = async (id) => {
+    const response = await fetch(urlApi + id, { method: "DELETE" });
+    console.log(urlApi + id);
+    if (response) {
+      setAmigos(amigos.filter((amigo) => amigo.id !== id));
+    }
+  };
+  const crearAmigo = useCallback(async () => {
+    const response = await fetch(urlApi, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nombre: nombre,
+        apellido: apellido,
+        valoracion: valoracion,
+      }),
+    });
+    if (response) {
+      llamadaAmigosApi(urlApi);
+    }
+  }, [apellido, nombre, valoracion]);
+  const modificarAmigo = useCallback(
+    async (id) => {
+      const response = await fetch(urlApi + id, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          nombre: nombre,
+          apellido: apellido,
+          valoracion: valoracion,
+        }),
+      });
+      if (response) {
+        llamadaAmigosApi(urlApi);
+      }
+    },
+    [apellido, nombre, valoracion]
+  );
+  const editarAmigo = (e) => {
+    e.preventDefault();
+    setEditar(true);
+    setModificar(false);
+    setNombre("");
+    setApellido("");
+    setValoracion(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="container vw-100">
+        <header>
+          <h1>Gestión de mis {numeroAmigos} amigos</h1>
+        </header>
+        <form className="form-group row align-items-center justify-content-between">
+          {editar && (
+            <Formulario
+              setNombre={setNombre}
+              setApellido={setApellido}
+              setValoracion={setValoracion}
+              crearAmigo={crearAmigo}
+              modificar={modificar}
+              nombre={nombre}
+              apellido={apellido}
+              valoracion={valoracion}
+            />
+          )}
+          <div className="col-3 row ">
+            {!editar && (
+              <button
+                className="boton rounded mb-3 w-100"
+                onClick={editarAmigo}
+              >
+                Crear
+              </button>
+            )}
+
+            {editar && (
+              <>
+                <button
+                  className="boton rounded mb-3 w-100"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    modificar ? modificarAmigo(id) : crearAmigo();
+                  }}
+                >
+                  {modificar ? "Modificar" : "Crear"}
+                </button>
+                <button className="boton rounded w-100" onClick={toggleEditar}>
+                  Cancelar
+                </button>
+              </>
+            )}
+          </div>
+        </form>
+
+        <section className="row ">
+          {amigos.map((amigo) => (
+            <Tarjeta
+              key={amigo.id}
+              amigo={amigo}
+              borrarAmigo={borrarAmigo}
+              setModificar={setModificar}
+              setEditar={setEditar}
+              setNombre={setNombre}
+              setApellido={setApellido}
+              setValoracion={setValoracion}
+              setId={setId}
+            />
+          ))}
+        </section>
+      </main>
+    </>
   );
 }
 
---
+++ src/componentes/Formulario.js
@@ -0,0 +1,54 @@
+export const Formulario = (props) => {
+  const {
+    setNombre,
+    setApellido,
+    setValoracion,
+    nombre,
+    apellido,
+    valoracion,
+  } = props;
+  return (
+    <>
+      <div className=" col-3">
+        <label className="form-text" htmlFor="nombre">
+          Nombre
+        </label>
+        <input
+          id="nombre"
+          className="form-control"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+      </div>
+      <div className="col-3" s>
+        <label className="form-text" htmlFor="apellido">
+          Apellido
+        </label>
+        <input
+          className="form-control"
+          id="apellido"
+          value={apellido}
+          onChange={(e) => setApellido(e.target.value)}
+        />
+      </div>
+      <div className="col-3" s>
+        <label className="form-text" htmlFor="valoración">
+          Valoración
+        </label>
+        <select
+          className="form-control"
+          id="valoración"
+          value={valoracion}
+          onChange={(e) => setValoracion(e.target.value)}
+        >
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </div>
+    </>
+  );
+};
